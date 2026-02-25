ce4aec4c3aaa10c5b9f488244bc689881df57340
allanhue <allanmwangi329@gmail.com>
2026-02-25 12:25:32 +0300

    golang fundamentals

--- slices.go
+++ slices.go
@@ -18,3 +18,21 @@ func main() {
   fmt.Printf("length = %d\n", len(myslice1))
   fmt.Printf("capacity = %d\n", cap(myslice1))
 }
+
+
+//operatoors go alows the derement and increment 
+++	Increment	Increases the value of a variable by 1	x++	
+--	Decrement	Decreases the value of a variable by 1	x--
+//logical operators
+&& 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10	
+|| 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
+!	Logical not	Reverse the result, returns false if the result is true
+
+
+//bitwise operators
+Operator	Name	Description	Example	T
+& 	AND	Sets each bit to 1 if both bits are 1	x & y	
+|	OR	Sets each bit to 1 if one of two bits is 1	x | y	
+ ^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ b	
+<<	Zero fill left shift	Shift left by pushing zeros in from the right	x << 2	
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
